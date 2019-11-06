6e343cc501ffd3a92b8121566edd033b854cfd3f
Rasmus Melberg <rasmusmelberg@gmail.com>
2019-11-06 16:29:07 +0100

    removed the phonenumber, and added af new as a string

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_06_123609) do
+ActiveRecord::Schema.define(version: 2019_11_06_152726) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2019_11_06_123609) do
     t.string "reviews"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phonenumber"
   end
 
   create_table "reviews", force: :cascade do |t|
